e2e0a5dc56859f527727daf6807b829a19775552
melamiyem <melamiyem.ce@gmail.com>
2015-04-28 17:05:14 +0300

    Revert "df"
    This reverts commit b40f70ff820108000af4d87f47417d95975ae90e.

--- build/generated/jax-wsCache/AlisWS_1/org/tempuri/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * AlisWS v2.32
- * 
- */
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://tempuri.org/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.tempuri;
